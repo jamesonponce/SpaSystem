9e0096c71ff32c9f7e19199f1858477008d12f9b
abcdoooy <abcdyambao22@gmail.com>
2018-10-19 15:06:08 +0800

    default

--- spafinal/resources/views/home.blade.php
+++ spafinal/resources/views/home.blade.php
@@ -1,23 +1,189 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+   <head>
+    <title>LykaRe of Wellness Center</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans" rel="stylesheet">
+</head>
+<body background="image/wood.jpg">
+    <!--nav-->
+    @if (Route::has('login'))
+    <nav class="stroke">
+        <div class="nav">
+            <div class="nav-header">
+                <ul>
+                
+                    @auth
+                    <li><a href="#home">HOME</a></li>
+                    @else
+                    <li><a href="#service">SERVICE</a></li>
+                    <li><a href="#about">ABOUT</a></li>
+                    
+                    <li><a href="{[ route('login') }}">LOGIN</a></li>
+                    
+                    
+                    
+                    @endauth
+                    
                     @endif
+                    
+                </ul>
 
-                    You are logged in!
-                </div>
             </div>
+        </div>
+     
+    </nav>
+    <!--videosection-->
+    <div class="video-section">
+      <video id="video-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> 
+        <br><source src="video/spa.mp4" type="video/mp4" style="height: 110%;">
+        Video not supported
+      </video>
+    </div>
+    <div style="background-color: #edf0c7; height: 250px;"><h6 style="font-family: 'Fira Sans', sans-serif; color:#ff5c00; font-size: 25px; text-align: center;"><br>About Us</h6><br>
+    <p style="font-family: 'Fira Sans', sans-serif; color: #4e9525; font-size: 16px;text-align: center;">All facial treatments use LykaRe skin care; a leader in Europe and north America for 60 years.<br> Products are an innovative mix of science and nature and are both paraben and cruelty-free. <br>Salon Riza & Day Spa offers the full line of Babor skin care products for take home purchase.</p>
+     <br><br>  <center><button class="button" style="vertical-align:middle"><span>See more </span></button></center>
+
+    
+    
+    </div>
+   
+    <br><br><br><div class="wrappery">
+        <div></div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+        <div class="hovereffect">
+        <img class="img-responsive" src="image/spa1.jpg" alt="" style="width: 340px; height: 275px; padding: 10px; ">
+        <div class="overlay"><br><br>
+            <h1 style="font-family: 'Fira Sans', sans-serif; color: antiquewhite; font-size: 18px;"> Body Massage</h1>
+            <p style="font-family: 'Fira Sans', sans-serif; color: #d4e8d4; font-size: 14px; padding: 20px;">During a therapeutic massage. Sessions typically last a minimum of 50 minutes which allows enough time to work over all the major areas of the body.</p>
+           <a class="info" href="#">Link here</a>
         </div>
     </div>
 </div>
-@endsection
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+        <div class="hovereffect">
+        <img class="img-responsive" src="image/facemassage.jpg" alt="" style="width: 340px; height: 275px; padding: 10px; ">
+        <div class="overlay">
+           <br> <br><h1 style="font-family: 'Fira Sans', sans-serif; color: antiquewhite; font-size: 18px;"> Face Massage</h1>
+            <p style="font-family: 'Fira Sans', sans-serif; color: #d4e8d4; font-size: 14px; padding: 20px;">During a therapeutic massage. Sessions typically last a minimum of 50 minutes which allows enough time to work over all the major areas of the body.</p>
+           <a class="info" href="#">Link here</a>
+        </div>
+    </div>
+</div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <img class="img-responsive" src="image/footmassage.jpg" alt="" style="width: 340px; height: 275px; padding: 10px; ">
+        <div class="overlay">
+            <br><br><h1 style="font-family: 'Fira Sans', sans-serif; color: antiquewhite; font-size: 18px;"> Foot Massage</h1>
+            <p style="font-family: 'Fira Sans', sans-serif; color: #d4e8d4; font-size: 14px; padding: 20px;">During a therapeutic massage. Sessions typically last a minimum of 50 minutes which allows enough time to work over all the major areas of the body.</p>
+           <a class="info" href="#">Link here</a>
+        </div>
+    </div>
+</div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <img class="img-responsive" src="image/pregnancymassage.jpg" alt="" style="width: 340px; height: 275px; padding: 10px; ">
+        <div class="overlay">
+           <br><br><h1 style="font-family: 'Fira Sans', sans-serif; color: antiquewhite; font-size: 18px;"> Pregnancy Massage</h1>
+            <p style="font-family: 'Fira Sans', sans-serif; color: #d4e8d4; font-size: 14px; padding: 20px;">During a therapeutic massage. Sessions typically last a minimum of 50 minutes which allows enough time to work over all the major areas of the body.</p>
+           <a class="info" href="#">Link here</a>
+        </div>
+    </div>
+</div>
+        <div></div>
+    </div>
+    
+        
+        
+    <div style="background-color: #edf0c7; height: 50px;"></div>
+    <div class="wrappery1">
+        <div></div>
+        <div id="slider">
+                <figure>
+                    <img class="wrappery1" style="padding: 10px;" src="image/17.jpg">
+                    <img class="wrappery1" style="padding: 10px;" src="image/room1.jpg">
+                    <img class="wrappery1" style="padding: 10px;" src="image/room2.jpg">
+                    <img class="wrappery1" style="padding: 10px;" src="image/17.jpg">
+                    <img class="wrappery1" style="padding: 10px;" src="image/room1.jpg">
+                </figure>
+            </div>
+         <div id="slider">
+                <figure>
+                    <img class="wrappery1" style="padding: 10px;" src="image/16.jpg">
+                    <img class="wrappery1" style="padding: 10px;" src="image/back2.jpg">
+                    <img class="wrappery1" style="padding: 10px;" src="image/back3.jpg">
+                    <img class="wrappery1" style="padding: 10px;" src="image/16.jpg">
+                    <img class="wrappery1" style="padding: 10px;" src="image/back2.jpg">
+                    </figure>
+            </div>
+             <div></div>
+        </div>
+        <div class="wrappery2">
+            <div></div>
+            <div id="slider">
+                <figure>
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/10.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/123.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/20.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/10.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/123.jpg">
+                </figure></div>
+            <div id="slider">
+                <figure>
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/2bb.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/back.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/banner1.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/foot34.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/fot35.jpg">
+                </figure> </div>
+            <div id="slider">
+                <figure>
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/l12.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/towel.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/ventosa12.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/ventosa13.jpg">
+                    <img class="wrappery2" style="padding: 10px;" src="image/images/ventosa2.jpg">
+                </figure></div>
+                <div></div>
+            </div>
+            
+            <div style="height:250px;background-image:image(wood.jpg);font-size:36px" id="portfolio"> 
+                <br>  <center> <div class="words" style="font-family: 'Fira Sans', sans-serif; font-size: 40px; color: #ff5c00  "> NewsLetter!</div>   
+                    <div class="words" style="font-family: 'Fira Sans', sans-serif; font-size: 20px; color: white  "> Get the latest news and updates on LykaRe Wellness Center Subscribe now!</div>  
+                        <input type="text" id="fname" name="fname" style="width: 450px; height: 50px; border-radius: 10px; ">
+                            <button class="button" style="vertical-align:middle; width: 150px;"><span>JOIN NOW!</span></button>
+                                </center> </div>
+           
+    <div style="background-color: #edf0c7; height: 230px;">
+    <div class="foooterz"> 
+        <div></div>
+        <div> <div class="lalagyan" style="font-family: 'Fira Sans', sans-serif; color: #ff5c00; font-size: 25px; padding-top: 35px; padding-left: 65px;">Location</div>
+        <div class="lalagyan" style="font-family: 'Fira Sans', sans-serif; color: #ff5c00; font-size: 25px; padding-left: 65px;"> LykaRe Wellness Center</div>
+        
+        <div class="lalagyan" style="font-family: 'Fira Sans', sans-serif; color: black; font-size: 19px; padding-left: 65px;">
+            Alabang, Muntinlupa City, Philippines
+            Email us: alabang@lykarewellnesscenter.com</div> </div>
+         
+        <div> <div class="lalagyan" style="font-family: 'Fira Sans', sans-serif; color: #ff5c00; font-size: 25px; padding-top: 35px; padding-left: 85px;"> To promote your products in lykarewellnesscenter.com's site, please contact our sales group: </div>
+            <div class="lalagyan" style="font-family: 'Fira Sans', sans-serif; color: black; font-size: 19px; padding-left: 85px;"> Phone: +63 (2) 369-2359 / 7895314
+Fax: +63 (2) 771-0143
+E-mail address: alabang@lykarewellnesscenter.com</div> 
+            </div>
+        <div> <div class="lalagyan" style="font-family: 'Fira Sans', sans-serif; color: #ff5c00; font-size: 25px; padding-top: 35px; padding-left: 75px;"> For event sponsorships: </div>
+            <div class="lalagyan" style="font-family: 'Fira Sans', sans-serif; color: black; font-size: 19px; padding-left: 75px;"> Please send proposals to alabang@lykarewellnesscenter.com</div>      
+        </div>
+        </div>
+    </div>
+    
+    <div class="footer" style=" position: relative; left: 0;  bottom: 0;  height: 40px;  width: 100%; background-color: #1b1b1c; color: white; text-align: center; padding-top: 10px;">  Copyright © 2018 LykaReWellnessCenter.com | About Us | Terms of Use | Email Us | FAQs </div>
+   
+  
+    
+
+</body>
+</html>
